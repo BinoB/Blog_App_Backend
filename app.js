579f442a721ad056ef25d9833678ca40cc2c6e1c
BinoB <binob831@gmail.com>
2023-07-25 10:04:43 +0530

    change cors

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 /* app.use(cors()); */
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-   /*  credentials: true, */
-  })
-);
+app.use(cors({
+  origin: "*",
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
