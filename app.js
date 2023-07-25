44cdd455c5c6d558003b265e1228fb791f348944
BinoB <binob831@gmail.com>
2023-07-25 09:56:00 +0530

    cors

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const app = express();
 /* app.use(cors()); */
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://blog-dcwf.onrender.com"],
-    credentials: true,
+    origin: ["http://localhost:3000"],
+   /*  credentials: true, */
   })
 );
 app.use(express.json());
